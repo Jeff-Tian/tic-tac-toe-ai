010dcef7c806263fcb71e1a1c51604fc6d5eaeec
Jeff Tian <jeff.tian@outlook.com>
2018-11-25 21:05:06 +0800

    Fix tests

--- src/test/Game.test.js
+++ src/test/Game.test.js
@@ -3,13 +3,13 @@ import Game from '../Game/Game';
 import Modes from "../Game/Modes";
 import PlayerO from '../Game/player-ai';
 import Judger from "../Game/Judger";
-import Enzyme, {shallow} from 'enzyme';
+import Enzyme, {mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({adapter: new Adapter()});
 
-test('Game can train players', () => {
-    let game = shallow(<Game/>);
+test('Game can train players', async () => {
+    let game = mount(<Game/>);
     expect(game.state()).toEqual({
         "OWeights": [0, 1, 1, 1],
         "XWeights": [],
@@ -29,8 +29,12 @@ test('Game can train players', () => {
         "xIsNext": true
     });
 
-    game.find('#silent-learn-button').simulate('click');
+    game.find('#turns').simulate('change', {target: {value: '1'}})
+    game.find('#start-auto-button').simulate('click');
 
+    await new Promise(resolve => setTimeout(resolve, 3000));
+
+    expect(game.state().history.length).toEqual(7);
 
     const gameProgress = Judger.gameProgress(new PlayerO('O', 'X', false).convertSquaresToBitmap(game.state().history[game.state().history.length - 1].squares));
     expect(!!(gameProgress.win || gameProgress.lost || gameProgress.fair)).toEqual(true);
