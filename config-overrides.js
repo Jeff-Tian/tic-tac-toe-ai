398d550661e1df70191a9cdedb9a9a1e27bd7baa
Jeff Tian <jeff.tian@outlook.com>
2018-12-08 19:51:19 +0800

    remount create-react-scripts

---
+++ config-overrides.js
@@ -0,0 +1,6 @@
+const {injectBabelPlugin} = require('react-app-rewired');
+module.exports = function override(config, env) {
+    // do stuff with the webpack config...
+    config = injectBabelPlugin(['import', {libraryName: 'antd-mobile', style: 'css'}], config);
+    return config;
+};
