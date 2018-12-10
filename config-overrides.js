543ae47a0f33fe7cf8c77bd23ba3875cf81c9385
Jeff Tian <jeff.tian@outlook.com>
2018-12-10 22:09:21 +0800

    recover test commands

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,9 @@
 const {injectBabelPlugin} = require('react-app-rewired');
 module.exports = function override(config, env) {
     // do stuff with the webpack config...
-    config = injectBabelPlugin(['import', {libraryName: 'antd-mobile', style: 'css'}], config);
+    config = injectBabelPlugin([
+        'import',
+        {libraryName: 'antd-mobile', style: 'css'}
+    ], config);
     return config;
 };
