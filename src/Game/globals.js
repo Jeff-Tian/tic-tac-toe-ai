f8a4023046ee664fdb057665b4993515db94fc21
Jeff Tian <jeff.tian@outlook.com>
2018-12-14 17:27:14 +0800

    alipay red package

--- src/Game/globals.js
+++ src/Game/globals.js
@@ -37,6 +37,15 @@ export default class Settings extends React.Component {
                     })
                 }
             </List>
+            <div>
+                <div>Icons made by <a href="https://www.freepik.com/" title="Freepik"
+                                      rel="noopener noreferrer">Freepik</a> from <a
+                    href="https://www.flaticon.com/" title="Flaticon" rel="noopener noreferrer"
+                    target="_blank">www.flaticon.com</a> is licensed by <a
+                    href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank"
+                    rel="noopener noreferrer">CC
+                    3.0 BY</a></div>
+            </div>
         </div>
     }
 }
--- src/result/result.js
+++ src/result/result.js
@@ -1,12 +1,41 @@
-import {Button, Flex} from "antd-mobile";
+import {Button, Flex, Modal} from "antd-mobile";
 import React from "react";
+import './result.css';
 
 export default class Result extends React.Component {
+    state = {showRedPackageModal: false}
     comfortPlayer = () => {
+        this.setState({
+            showRedPackageModal: true
+        })
+    }
+
+    onClose = () => {
+        this.setState({
+            showRedPackageModal: false
+        })
     }
 
     render() {
         return <div>
+
+            <Modal
+                visible={this.state.showRedPackageModal}
+                transparent
+                maskClosable={false}
+                title="拿红包来拯救你"
+                footer={[{
+                    text: '领过了，开心多啦！', onPress: () => {
+                        this.onClose();
+                    }
+                }]}
+            >
+                <div style={{height: 500, overflow: 'auto', width: 350}}>
+                    <img src={require('../Resources/images/alipay-red-package.png')} alt="支付宝红包"
+                         style={{maxWidth: '90%'}}/>
+                </div>
+            </Modal>
+
             {
                 this.props.winnerInfo.who === 'X' &&
                 <Flex>
@@ -19,7 +48,7 @@ export default class Result extends React.Component {
                 this.props.winnerInfo.who === 'O' &&
                 <Flex>
                     <Flex.Item>
-                        <Button onClick={this.comfortPlayer}>输掉了，不开心。</Button>
+                        <Button icon={require('../icons/crying.svg')} onClick={this.comfortPlayer}>输掉了，不开心。</Button>
                     </Flex.Item>
                 </Flex>
             }
