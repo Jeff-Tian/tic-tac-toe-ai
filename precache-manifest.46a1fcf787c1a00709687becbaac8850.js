921328e15ebe69c7cf297dac77abeda3533dd5f2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-10 16:31:59 +0000

    Deploy tictactoe.js.org to github.com/Jeff-Tian/tic-tac-toe-ai.git:gh-pages

---
+++ precache-manifest.46a1fcf787c1a00709687becbaac8850.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9685a6e145e7c7c2a10ef0a3f5f7aefd",
+    "url": "/alipay-red-package.html"
+  },
+  {
+    "revision": "463eec1c488f25bc123b00165d8bf9ef",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/.cache"
+  },
+  {
+    "revision": "64247f7559d1c60887b8b941748d0c05",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/android-chrome-144x144.png"
+  },
+  {
+    "revision": "aa5a31a12db016881ef83922cd473172",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/android-chrome-192x192.png"
+  },
+  {
+    "revision": "5d1d3b191538bb20c6a27554d1cece1d",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/android-chrome-256x256.png"
+  },
+  {
+    "revision": "32c9cbf7e618096d0a882217e616580e",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/android-chrome-36x36.png"
+  },
+  {
+    "revision": "11a0d70a63fb23c60ee107b55c5b57e7",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/android-chrome-384x384.png"
+  },
+  {
+    "revision": "578db0a110c13638fe2297fbeb97c03d",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/android-chrome-48x48.png"
+  },
+  {
+    "revision": "29e4cf12d36ebf4860ad31db4897badb",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/android-chrome-512x512.png"
+  },
+  {
+    "revision": "1ed36257ce869b5b807a2a8511c8d968",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/android-chrome-72x72.png"
+  },
+  {
+    "revision": "35b74f3e8f825a1c82247fc9d4f712fa",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/android-chrome-96x96.png"
+  },
+  {
+    "revision": "d03cb911e905672ddefcb7fadcb018f1",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-icon-114x114.png"
+  },
+  {
+    "revision": "4349613a992441ce1c32745c4aa794a3",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "66c456b79e92f9beecb2a90502873d55",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-icon-144x144.png"
+  },
+  {
+    "revision": "5f8a78de868cff3741894be930a2143b",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "8ab35cf17e0152065c5b7fd4aea1b93c",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-icon-167x167.png"
+  },
+  {
+    "revision": "c7dd577aaf5e759a1bdcc668aa2f63bc",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "7ecbb56e5050ea129d5ef12245bb2142",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-icon-57x57.png"
+  },
+  {
+    "revision": "5460c16cede60eb508a3ca765649ac0a",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "5f4ec7662421955f32ebb8dae201c168",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-icon-72x72.png"
+  },
+  {
+    "revision": "19334e6a43b2c3d31bb6de193f28a586",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "c7dd577aaf5e759a1bdcc668aa2f63bc",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-icon-precomposed.png"
+  },
+  {
+    "revision": "c7dd577aaf5e759a1bdcc668aa2f63bc",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-icon.png"
+  },
+  {
+    "revision": "84a467e0250628546758608c79a57c81",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-startup-image-1182x2208.png"
+  },
+  {
+    "revision": "b76064010510cf3aa4333e084c642831",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-startup-image-1242x2148.png"
+  },
+  {
+    "revision": "172083f64c9c82aedf9c7e7d84c63f83",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-startup-image-1496x2048.png"
+  },
+  {
+    "revision": "fa634f2f357424cb3141081bab9624ff",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-startup-image-1536x2008.png"
+  },
+  {
+    "revision": "421cbcbb2c4bc1f95f194dcea8b3f49a",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-startup-image-320x460.png"
+  },
+  {
+    "revision": "2d338074e8b98fd92a947b3c0cace714",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-startup-image-640x1096.png"
+  },
+  {
+    "revision": "0321bc9ca30cd3d9fc3bc6c39729878e",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-startup-image-640x920.png"
+  },
+  {
+    "revision": "a4404759d23649b838572eefc11703d3",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-startup-image-748x1024.png"
+  },
+  {
+    "revision": "6cca157ff16d7f0d275a8a432bd1830e",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-startup-image-750x1294.png"
+  },
+  {
+    "revision": "888561212803ff16e67b4f3485fd08df",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/apple-touch-startup-image-768x1004.png"
+  },
+  {
+    "revision": "6b9febff1eb49f1662476afc3e8c6d77",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/browserconfig.xml"
+  },
+  {
+    "revision": "2f774020eac3c70c0a489e082ba4baa3",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/favicon-16x16.png"
+  },
+  {
+    "revision": "aa7907f548f9ce4b9cd91ab0d04f3289",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/favicon-32x32.png"
+  },
+  {
+    "revision": "d68f6cf4a5d8ccfe71161adb4ee46d73",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/favicon.ico"
+  },
+  {
+    "revision": "4779f10b832b048d4f88660b79abfa8b",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/firefox_app_128x128.png"
+  },
+  {
+    "revision": "12bce34d7b3ac87ee15847ffcfb23940",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/firefox_app_512x512.png"
+  },
+  {
+    "revision": "f915bb9b0fc173c2817f3eb118134b17",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/firefox_app_60x60.png"
+  },
+  {
+    "revision": "4c294f47a18b4df10c18af7fb879c60c",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/manifest.json"
+  },
+  {
+    "revision": "6a8e29ade8e481d7185fef5fe8428ca4",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/manifest.webapp"
+  },
+  {
+    "revision": "64247f7559d1c60887b8b941748d0c05",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/mstile-144x144.png"
+  },
+  {
+    "revision": "ef2da25108fe5f36a5293461fb0c8a85",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/mstile-150x150.png"
+  },
+  {
+    "revision": "3c9361449d3696e4bbdab3290bf48e4c",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/mstile-310x150.png"
+  },
+  {
+    "revision": "2ac191d2063e8a7a900b72401f7f3e98",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/mstile-310x310.png"
+  },
+  {
+    "revision": "546e221789f93428100b2941fdf73725",
+    "url": "/icons-589a6270e00dd181c247490d781b04ae/mstile-70x70.png"
+  },
+  {
+    "revision": "8ceaa63b9273be63cc0f7e2d32511ce7",
+    "url": "/iconstats-a37e3a43a02f5655a2c7.json"
+  },
+  {
+    "revision": "9685a6e145e7c7c2a10ef0a3f5f7aefd",
+    "url": "/identified.html"
+  },
+  {
+    "revision": "b01b1da257f8b358e3c6767cc955e27f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9685a6e145e7c7c2a10ef0a3f5f7aefd",
+    "url": "/settings.html"
+  },
+  {
+    "revision": "1fe802393f602a10cc67",
+    "url": "/static/css/1.fca9bb22.chunk.css"
+  },
+  {
+    "revision": "c4459c04deb4c30c532c",
+    "url": "/static/css/main.ae9e5557.chunk.css"
+  },
+  {
+    "revision": "1fe802393f602a10cc67",
+    "url": "/static/js/1.1fe80239.chunk.js"
+  },
+  {
+    "revision": "c4459c04deb4c30c532c",
+    "url": "/static/js/main.c4459c04.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a983b7291b7fb9dc0cfe579d19bdddcf",
+    "url": "/static/media/alipay-receive-money.a983b729.jpg"
+  },
+  {
+    "revision": "5cd76bdb498d84511f32b30cbb989a01",
+    "url": "/static/media/alipay-red-package.5cd76bdb.png"
+  },
+  {
+    "revision": "993670b80759c26b3a8b4afdc7bbefbd",
+    "url": "/static/media/crying.993670b8.svg"
+  },
+  {
+    "revision": "9dc154d29ab2c47ccb160f5f78ccd598",
+    "url": "/static/media/user.9dc154d2.svg"
+  },
+  {
+    "revision": "9685a6e145e7c7c2a10ef0a3f5f7aefd",
+    "url": "/support-author.html"
+  }
+]);
