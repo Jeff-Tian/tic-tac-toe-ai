92962860a757e50dc284307f68a7a970c793131a
Jeff Tian <jeff.tian@outlook.com>
2024-01-07 14:58:57 +0800

    fix: add custom-cra to fix build error

--- config-overrides.js
+++ config-overrides.js
@@ -1,61 +1,62 @@
-const { injectBabelPlugin } = require("react-app-rewired");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const paths = require("react-scripts/config/paths");
-let FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
-module.exports = function override(config, env) {
-  // do stuff with the webpack config...
-  config = injectBabelPlugin(
-    ["import", { libraryName: "antd-mobile", style: "css" }],
-    config
-  );
+const {override, fixBabelImports, addWebpackPlugin} = require('customize-cra');
 
-  let pages = [
+const pages = [
     "settings",
     "alipay-red-package",
     "support-author",
     "identified",
     "index"
-  ].map(
+].map(
     f =>
-      new HtmlWebpackPlugin({
-        filename: `${f}.html`,
-        template: paths.appHtml,
-        inject: true,
-        minify: {
-          removeComments: true,
-          collapseWhitespace: true,
-          removeRedundantAttributes: true,
-          useShortDoctype: true,
-          removeEmptyAttributes: true,
-          removeStyleLinkTypeAttributes: true,
-          keepClosingSlash: true,
-          minifyJS: true,
-          minifyCSS: true,
-          minifyURLs: true
-        },
-        chunks: "all"
-      })
-  );
+        new HtmlWebpackPlugin({
+            filename: `${f}.html`,
+            template: paths.appHtml,
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true
+            },
+            chunks: "all"
+        })
+);
 
-  config.plugins = pages.concat(config.plugins).concat([
-    new FaviconsWebpackPlugin({
-      logo: "./public/images/screenshot.png",
-      title: "Tic Tac Toe AI",
-      icons: {
-        android: true,
-        appleIcon: true,
-        appleStartup: true,
-        coast: false,
-        favicons: true,
-        firefox: true,
-        opengraph: true,
-        twitter: true,
-        yandex: false,
-        windows: true
-      }
-    })
-  ]);
+const addCustomize = override(
+    addWebpackPlugin(
+        new FaviconsWebpackPlugin({
+            logo: "./public/images/screenshot.png",
+            title: "Tic Tac Toe AI",
+            icons: {
+                android: true,
+                appleIcon: true,
+                appleStartup: true,
+                coast: false,
+                favicons: true,
+                firefox: true,
+                opengraph: true,
+                twitter: true,
+                yandex: false,
+                windows: true
+            }
+        }),
+    ),
+    ...pages.map(p => addWebpackPlugin(p)),
+    fixBabelImports('import', {
+        libraryName: 'antd-mobile',
+        style: 'css',
+    }),
+);
 
-  return config;
-};
+
+module.exports = addCustomize;
